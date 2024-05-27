382d04d2360c7a8ebde3c6ee51e89567b62d86c9
Luis Felipe Maciel Honorato <politica.de.honorato@outlook.com>
2024-05-27 18:11:33 -0400

    feat: create api_keys file

---
+++ lib/api_keys.dart
@@ -0,0 +1,8 @@
+// By default all files related to Api Keys must be added in .gitgnore,
+// but in this project, to employers' testability reasons, it will not be added
+// on .gitgnore, but if it was a real production app, it would be in gitgnore.
+
+/// Api Keys used in project.
+class ApiKeys {
+  static const nasaApiKey = 'pejaih922qYbtoR4Sbh5yCS2lOtBqvXkcZow9tVV';
+}
