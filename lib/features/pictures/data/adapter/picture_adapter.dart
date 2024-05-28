6cf5780dc81ecf3ee807ea078db25cf505503f73
Luis Felipe Maciel Honorato <politica.de.honorato@outlook.com>
2024-05-28 00:27:40 -0400

    adjust: implement validation to midia types differents of images

--- lib/features/pictures/data/adapter/picture_adapter.dart
+++ lib/features/pictures/data/adapter/picture_adapter.dart
@@ -4,17 +4,22 @@ class PictureAdapter {
   const PictureAdapter._();
 
   static List<PictureEntity> fromJson(List<Map<String, dynamic>> json) {
-    return json
-        .map(
-          (pictureJson) => PictureEntity(
-            url: pictureJson['url'],
-            hdUrl: pictureJson['hdurl'],
-            date: pictureJson['date'],
-            title: pictureJson['title'],
-            description: pictureJson['explanation'],
-            copyRight: pictureJson['copyright'],
-          ),
-        )
-        .toList();
+    final List<PictureEntity> picturesList = [];
+    for (Map<String, dynamic> pictureJson in json) {
+      if (pictureJson['media_type'] != 'image') {
+        continue;
+      }
+      picturesList.add(
+        PictureEntity(
+          url: pictureJson['url'],
+          hdUrl: pictureJson['hdurl'],
+          date: pictureJson['date'],
+          title: pictureJson['title'],
+          description: pictureJson['explanation'],
+          copyRight: pictureJson['copyright'],
+        ),
+      );
+    }
+    return picturesList;
   }
 }
